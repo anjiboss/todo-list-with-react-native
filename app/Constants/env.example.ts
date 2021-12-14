fc040a74ec15fc9ea54032c88a22c505f677e102
Anji <nhu.nhu.face@gmail.com>
2021-12-14 14:05:51 +0900

    fix env

--- app/Constants/env.example.ts
+++ app/Constants/env.example.ts
@@ -1,4 +1,3 @@
 export const environment = {
-  // API_URL: "http://localhost:5000/v1/api",
-  API_URL: "https://cryptic-escarpment-08718.herokuapp.com/v1/api",
+  API_URL: "http://localhost:5000/v1/api",
 };

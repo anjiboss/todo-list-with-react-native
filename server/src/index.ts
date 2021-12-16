dd198d9cc753c525e014ee5478e9f2f708958878
Anji <nhu.nhu.face@gmail.com>
2021-12-16 21:45:31 +0900

    add cors and return todo on add-todo route

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,11 @@
 import "reflect-metadata";
 import { createConnection } from "typeorm";
 import express from "express";
+import cors from 'cors'
 
 const app = express();
 app.use(express.json());
+app.use(cors())
 const PORT = process.env.PORT || 5000;
 //  --------------------------------Import Routers
 import { userRoute } from "./Routes/user";
